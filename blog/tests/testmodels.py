c7ecd2bbdac132f69e474cc3635eb20469087f59
Thierry Chappuis <thierry@chappuis.io>
2018-11-13 11:57:31 +0100

    initial commit

---
+++ blog/tests/testmodels.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from blog.models import Article
+
+# models test
+class BlogModelTest(TestCase):
+
+    def create_article(self, title="only a test", content="yes, this is only a test"):
+        return Article.objects.create(title=title, content=content)
+
+    def test_article_creation(self):
+        article = self.create_article()
+        self.assertTrue(isinstance(article, Article))
+        self.assertEqual(str(article), article.title)
